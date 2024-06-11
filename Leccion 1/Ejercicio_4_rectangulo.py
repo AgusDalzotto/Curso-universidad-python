08e314d88f7c74e28b030a3d19c8f300e7464f21
Agustin Dalzotto <agusdalzotto@gmail.com>
2024-06-11 10:49:25 -0300

    adding

--- Leccion 1/Ejercicio_4_rectangulo.py
+++ Leccion 1/Ejercicio_4_rectangulo.py
@@ -6,5 +6,10 @@ ancho = int(input("proporcione el ancho del rectangulo:"))
 #operaciones
 
 area = alto * ancho
-perimetro
+perimetro = (alto + ancho) * 2
+
+#testeo
+
+print("El area del rectangulo es:", area)
+print("El perimtro del rectangulo es:", perimetro)
 
