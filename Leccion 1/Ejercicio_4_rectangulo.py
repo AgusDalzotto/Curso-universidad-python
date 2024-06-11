ba48bb031243a7065373be9bcb5582509e332ba8
Agustin Dalzotto <agusdalzotto@gmail.com>
2024-06-11 10:47:44 -0300

    Exercise 4

---
+++ Leccion 1/Ejercicio_4_rectangulo.py
@@ -0,0 +1,10 @@
+#Ejercicio4
+
+alto = int(input("proporcione el alto del rectangulo:"))
+ancho = int(input("proporcione el ancho del rectangulo:"))
+
+#operaciones
+
+area = alto * ancho
+perimetro
+
