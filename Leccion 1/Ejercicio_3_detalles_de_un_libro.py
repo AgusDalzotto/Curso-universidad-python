509845c50f993ea2f30dd254d42cc057d13ed6d0
Agustin Dalzotto <agusdalzotto@gmail.com>
2024-06-11 10:28:17 -0300

    Exercise 3

--- Leccion 1/Ejercicio_3_detalles_de_un_libro.py
+++ Leccion 1/Ejercicio_3_detalles_de_un_libro.py
@@ -1,1 +1,8 @@
-#Ejercicio 3
+#Ejercicio3
+
+titulo = input("proporciona el título del libro:")
+autor = input("proporciona el autor del libro:")
+
+#testeo
+
+print(titulo, "fue escrito por", autor)
